c71071fbbb7dbf9441596bb7586925bdb898ce79
evgeshaDoc <kolosmanyc@gmail.com>
2023-02-26 15:17:21 +0300

    libs: config updated import

--- libs/config/src/index.ts
+++ libs/config/src/index.ts
@@ -1,22 +1,22 @@
-import { resolve } from 'path'
+import { resolve } from "path";
 
-import dotenv from 'dotenv'
-import { cleanEnv, str } from 'envalid'
+import * as dotenv from "dotenv";
+import { cleanEnv, str } from "envalid";
 
 try {
-    dotenv.config({ path: resolve(process.cwd(), '.env') })
+	dotenv.config({ path: resolve(process.cwd(), "..", "..", ".env") });
 } catch {}
 
 const config = cleanEnv(process.env, {
-    POSTGRES_USER: str({ default: 'postgres' }),
-    POSTGRES_PASSWORD: str({ default: 'postgres' }),
-    POSTGRES_DB: str({ default: 'telman' }),
-    POSTGRES_HOST: str({ default: 'postgres' }),
-    POSTGRES_PORT: str({ default: '5432' }),
-    USERS_QUEUE: str({ default: 'USERS_QUEUE' }),
-    AUTH_QUEUE: str({ default: 'AUTH_QUEUE' }),
-    KAFKA_BROKERS: str({ default: 'kafka:9571' }),
-    DOPPLER_TOKEN: str(),
-})
+	POSTGRES_USER: str({ default: "postgres" }),
+	POSTGRES_PASSWORD: str({ default: "postgres" }),
+	POSTGRES_DB: str({ default: "telman" }),
+	POSTGRES_HOST: str({ default: "postgres" }),
+	POSTGRES_PORT: str({ default: "5432" }),
+	USERS_QUEUE: str({ default: "USERS_QUEUE" }),
+	AUTH_QUEUE: str({ default: "AUTH_QUEUE" }),
+	KAFKA_BROKERS: str({ default: "kafka:9571" }),
+	DOPPLER_TOKEN: str(),
+});
 
-export default config
+export default config;
